520cadfdc93452c52a2a438582aef5be98f14573
Victor <v.safsten@gmail.com>
2024-10-16 14:06:46 +0200

    Improve login logout user experience

--- src/components/layout/ConfiguredRoute.tsx
+++ src/components/layout/ConfiguredRoute.tsx
@@ -1,21 +1,51 @@
-import React, { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import useSelectSession from "../../hooks/useSelectSession";
 import { useAuth } from "../../provider/authProvider";
+import { ListSessionsResponse } from "../../types/Session";
+import { callAPI } from "../../utils/apiService";
+import Loading from "../common/Loading/Loading";
 
 export const ConfiguredRoute: React.FC = () => {
   const navigate = useNavigate();
   const { user } = useAuth();
-  const { currentSession } = useSelectSession();
+  const { currentSession, selectSession } = useSelectSession();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["sessions"],
+    enabled: !!user,
+    queryFn: () => callAPI<ListSessionsResponse>("/sessions", "GET"),
+  });
 
   useEffect(() => {
+    // If user is null, navigate to login
     if (user === null) {
       navigate("/login", { replace: true });
+      return;
     }
-    if (currentSession === null) {
-      navigate("/configuration");
-    }
-  }, [user, navigate]);
 
-  return user && currentSession ? <Outlet /> : null;
+    // If data is still loading, do nothing
+    if (isLoading) {
+      return;
+    }
+
+    // If there's an error, navigate to configuration
+    if (error || (data && data.length === 0)) {
+      navigate("/configuration");
+      return;
+    }
+
+    // If no current session is selected and data is available, select the latest session
+    if (currentSession === null && data && data.length > 0) {
+      selectSession(data[data.length - 1]);
+    }
+  }, [user, currentSession, data, error, isLoading, navigate, selectSession]);
+
+  // Show loading spinner if user or session is not ready
+  if (isLoading || !user || !currentSession) {
+    return <Loading />;
+  }
+
+  // If user and session are ready, render the child components
+  return <Outlet />;
 };
