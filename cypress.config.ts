0aa24af08410e4299f56d71d4cf43af850c4732e
Victor <v.safsten@gmail.com>
2024-10-21 16:38:29 +0200

    Test different github actions approach

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:80",
     chromeWebSecurity: false,
-    defaultCommandTimeout: 30000, // 30 seconds
-    requestTimeout: 30000, // 30 seconds
+    defaultCommandTimeout: 10000, // 10 seconds
+    requestTimeout: 10000, // 10 seconds
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
